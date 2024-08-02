407838d8a65eb4b092f914355cc25c77268a8db5
samuel-santinelli <samuelsantinelli@gmail.com>
2024-08-02 12:39:49 -0300

    feature(expo-scure) - Creating the Default spinner component

---
+++ src/components/DefaultSpinner.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { Fold } from "react-native-animated-spinkit";
+
+const DefaultSpinner = () => {
+  const colorsArray = [
+    "#FF007A",
+    "#FD0101",
+    "#FFD700",
+    "#6B8E23",
+    "#4B0082",
+    "#D9D9D9",
+    "#9ACD32",
+    "#ADFF2F",
+    "#09BAE1",
+  ];
+
+  const getRandomColor = (currentColor) => {
+    let newColor;
+    do {
+      newColor = colorsArray[Math.floor(Math.random() * colorsArray.length)];
+    } while (newColor === currentColor);
+    return newColor;
+  };
+
+  const [currentColor, setCurrentColor] = useState(getRandomColor(null));
+
+  useEffect(() => {
+    const colorInterval = setInterval(() => {
+      setCurrentColor((prevColor) => getRandomColor(prevColor));
+    }, 2000);
+
+    return () => clearInterval(colorInterval);
+  }, []);
+  return <Fold size={80} color={currentColor} />;
+};
+
+export default DefaultSpinner;
