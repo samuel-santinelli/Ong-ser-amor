3e727c39945fdb1858019c925d0b77817f4b4f5f
samuel-santinelli <samuelsantinelli@gmail.com>
2024-09-13 15:20:37 -0300

    feature(DefaultButton) - Changing the default button component

--- src/components/DefaultButton.jsx
+++ src/components/DefaultButton.jsx
@@ -3,8 +3,8 @@ import { Text } from "react-native";
 import { Button } from "react-native-paper";
 
 const DefaultButton = ({ onPress, title, style, icon, mode, loading, loadingText, textColor}) => (
-  <Button
-    icon={icon}
+  <Button  
+    icon={icon}    
     mode={mode}
     onPress={onPress}
     style={style}
