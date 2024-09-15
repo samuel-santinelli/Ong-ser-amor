b0ff1fcf3c26722ea3dffa113ce104884e000ea8
samuel-santinelli <samuelsantinelli@gmail.com>
2024-09-15 08:56:33 -0300

    feature(DefaultTitle) - Changing the DefaultTitle component

--- src/components/DefaultTitle.jsx
+++ src/components/DefaultTitle.jsx
@@ -1,13 +1,12 @@
 import { StyleSheet, Text } from "react-native";
 
-const DefaultTitle = ({ title }) => {
-  return <Text style={styles.title}>{title}</Text>;
+const DefaultTitle = ({ title, color }) => {
+  return <Text style={[styles.title, { color: color ? color : "#ff007a"}]}>{title}</Text>;
 };
 
 const styles = StyleSheet.create({
   title: {
-    fontSize: 30,
-    color: "#ff007a",
+    fontSize: 26,    
     textAlign: "center",
     fontFamily: "Montserrat-Bold",
   },
