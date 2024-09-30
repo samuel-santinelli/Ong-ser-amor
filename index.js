761619dae0b96c267303a925dfa4c9f7930b25e5
samuel-santinelli <samuelsantinelli@gmail.com>
2024-09-30 16:11:45 -0300

    feature(ColorTheme) - Implementing the color dinamic in index.js

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 // App.tsx
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { PaperProvider, MD3LightTheme, adaptNavigationTheme } from 'react-native-paper';
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+  PaperProvider,
+  MD3LightTheme,
+  adaptNavigationTheme,
+} from "react-native-paper";
 const Stack = createStackNavigator();
-const { LightTheme } = adaptNavigationTheme({ reactNavigationLight: DefaultTheme });
-import HomeScreen from './src/screens/HomeScreen';
-import DetailsScreen from './src/screens/DetailsScreen';
+const { LightTheme } = adaptNavigationTheme({
+  reactNavigationLight: DefaultTheme,
+});
+import HomeScreen from "./src/screens/HomeScreen";
+import DetailsScreen from "./src/screens/DetailsScreen";
 // Only import react-native-gesture-handler on native platforms
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const primaryColor = useSelector((state) => state.theme.primaryColor);
+  console.log("primary color", primaryColor);
+  
+
   const theme = {
     ...DefaultTheme,
-    fonts: {
-      regular: {
-        fontFamily: 'Montserrat-Regular'
-      },
-      medium: {
-        fontFamily: 'Montserrat-Medium'
-      },
-      light: {
-        fontFamily: 'Montserrat-Light'
-      },
-      thin: {
-        fontFamily: 'Montserrat-Thin'
-      },
+    colors: {
+      ...DefaultTheme.colors,
+      primary: primaryColor,
     },
   };
+
   return (
     <PaperProvider theme={theme}>
-      <NavigationContainer theme={LightTheme}>
+      <NavigationContainer theme={theme}>
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Details" component={DetailsScreen} />
